19727b8c5bab02cf5a1a610a8e46265a1b337b9c
github-action-benchmark <github@users.noreply.github.com>
2025-06-19 00:18:45 +0000

    add Criterion.rs Benchmark (customSmallerIsBetter) benchmark result for 828bad33924cda1f534fc7fb7be03c3555d44caf

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1750206505399,
+  "lastUpdate": 1750292325692,
   "repoUrl": "https://github.com/dblnz/runwasi",
   "entries": {
     "Criterion.rs Benchmark": [
@@ -12946,6 +12946,52 @@ window.BENCHMARK_DATA = {
             "value": 89787313,
             "range": "± 2427177",
             "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Jiaxiao Zhou",
+            "username": "Mossaka",
+            "email": "duibao55328@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "828bad33924cda1f534fc7fb7be03c3555d44caf",
+          "message": "Merge pull request #872 from containerd/dependabot/github_actions/sigstore/cosign-installer-3.8.1\n\nchore(deps): bump sigstore/cosign-installer from 3.8.0 to 3.8.1",
+          "timestamp": "2025-02-25T23:48:04Z",
+          "url": "https://github.com/dblnz/runwasi/commit/828bad33924cda1f534fc7fb7be03c3555d44caf"
+        },
+        "date": 1750292323857,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "wamr/memory-usage",
+            "value": 15188,
+            "unit": "kB",
+            "extra": "shim: 12456 kB\nzygote: 2732 kB"
+          },
+          {
+            "name": "wasmedge/memory-usage",
+            "value": 65428,
+            "unit": "kB",
+            "extra": "shim: 53072 kB\nzygote: 12356 kB"
+          },
+          {
+            "name": "wasmer/memory-usage",
+            "value": 18348,
+            "unit": "kB",
+            "extra": "shim: 15064 kB\nzygote: 3284 kB"
+          },
+          {
+            "name": "wasmtime/memory-usage",
+            "value": 18948,
+            "unit": "kB",
+            "extra": "shim: 15812 kB\nzygote: 3136 kB"
           }
         ]
       }
